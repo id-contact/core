d9461236369cb6c4e728a38bdb123a730f0d1866
David Venhoek <david@tweedegolf.com>
2021-04-13 10:26:46 +0200

    Modified configuration to use rocket configuration system.

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,8 @@ extern crate rocket;
 use config::CoreConfig;
 use methods::auth_attr_shim;
 use options::session_options;
+use rocket::fairing::AdHoc;
 use start::{session_start_auth_only, session_start_full, start_session_comm_only};
-use std::env;
 
 #[launch]
 fn boot() -> rocket::Rocket {
@@ -21,16 +21,19 @@ fn boot() -> rocket::Rocket {
     ))))
     .expect("failure to setup loggin");
 
-    let config_filename = env::var("IDC_CORE_CONFIG_FILE")
-        .expect("No config file path defined, please set IDC_CORE_CONFIG_FILE");
-
-    rocket(CoreConfig::from_file(&config_filename))
+    let base = setup_routes(rocket::ignite());
+    let config = base
+        .figment()
+        .extract::<CoreConfig>()
+        .expect("Could not parse configuration");
+    match config.sentry_dsn() {
+        Some(dsn) => base.attach(sentry::SentryFairing::new(dsn)),
+        None => base,
+    }
 }
 
-fn rocket(config: CoreConfig) -> rocket::Rocket {
-    let sentry_fairing = config.sentry_dsn().map(|dsn| sentry::SentryFairing::new(dsn));
-
-    let base = rocket::ignite().manage(config).mount(
+fn setup_routes(base: rocket::Rocket) -> rocket::Rocket {
+    base.mount(
         "/",
         routes![
             session_options,
@@ -39,10 +42,6 @@ fn rocket(config: CoreConfig) -> rocket::Rocket {
             start_session_comm_only,
             auth_attr_shim,
         ],
-    );
-
-    match sentry_fairing {
-        Some(sentry) => base.attach(sentry),
-        None => base,
-    }
+    )
+    .attach(AdHoc::config::<CoreConfig>())
 }
--- src/options.rs
+++ src/options.rs
@@ -67,65 +67,69 @@ mod tests {
     use rocket::{http::Status, local::blocking::Client};
 
     use super::SessionOptions;
-    use crate::config::CoreConfig;
-    use crate::rocket;
+    use crate::setup_routes;
+    use figment::providers::{Format, Toml};
+    use rocket::figment::Figment;
 
     const TEST_CONFIG_VALID: &'static str = r#"
-server_url: https://core.idcontact.test.tweede.golf
-internal_url: http://core:8000
-internal_secret: sample_secret_1234567890178901237890
+[global]
+server_url = "https://core.idcontact.test.tweede.golf"
+internal_url = "http://core:8000"
+internal_secret = "sample_secret_1234567890178901237890"
 
-auth_methods:
-  - tag: irma
-    name: Gebruik je IRMA app
-    image_path: /static/irma.svg
-    start: http://auth-irma:8000
-  - tag: digid
-    name: Gebruik DigiD
-    image_path: /static/digid.svg
-    start: http://auth-test:8000
 
-comm_methods:
-  - tag: call
-    name: Bellen
-    image_path: /static/phone.svg
-    start: http://comm-test:8000
-  - tag: chat
-    name: Chatten
-    image_path: /static/chat.svg
-    start: http://comm-matrix-bot:3000
+[[global.auth_methods]]
+tag = "irma"
+name = "Gebruik je IRMA app"
+image_path = "/static/irma.svg"
+start = "http://auth-irma:8000"
 
-purposes:
-  - tag: report_move
-    attributes:
-      - email
-    allowed_auth:
-      - "*"
-    allowed_comm:
-      - call
-      - chat
-  - tag: request_permit
-    attributes:
-      - email
-    allowed_auth:
-      - irma
-      - digid
-    allowed_comm:
-      - "*"
-  - tag: request_passport
-    attributes:
-      - email
-    allowed_auth:
-      - irma
-    allowed_comm:
-      - call
+[[global.auth_methods]]
+tag = "digid"
+name = "Gebruik DigiD"
+image_path = "/static/digid.svg"
+start = "http://auth-test:8000"
+
+
+[[global.comm_methods]]
+tag = "call"
+name = "Bellen"
+image_path = "/static/phone.svg"
+start = "http://comm-test:8000"
+
+[[global.comm_methods]]
+tag = "chat"
+name = "Chatten"
+image_path = "/static/chat.svg"
+start = "http://comm-matrix-bot:3000"
+
+
+[[global.purposes]]
+tag = "report_move"
+attributes = [ "email" ]
+allowed_auth = [ "*" ]
+allowed_comm = [ "call", "chat" ]
+
+[[global.purposes]]
+tag = "request_permit"
+attributes = [ "email" ]
+allowed_auth = [ "irma", "digid" ]
+allowed_comm = [ "*" ]
+
+[[global.purposes]]
+tag = "request_passport"
+attributes = [ "email" ]
+allowed_auth = [ "irma" ]
+allowed_comm = [ "call" ]
 
 "#;
     #[test]
     fn test_options() {
-        let config = CoreConfig::from_str(TEST_CONFIG_VALID);
+        let figment = Figment::from(rocket::Config::default())
+            .select(rocket::Config::DEFAULT_PROFILE)
+            .merge(Toml::string(TEST_CONFIG_VALID).nested());
 
-        let client = Client::tracked(rocket(config)).unwrap();
+        let client = Client::tracked(setup_routes(rocket::custom(figment))).unwrap();
 
         let response = client.get("/session_options/report_move").dispatch();
         assert_eq!(response.status(), Status::Ok);
