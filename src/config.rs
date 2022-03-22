2467ed74e6e6ee6fedec857b467759425040da1b
Arjen <4867268+arjentz@users.noreply.github.com>
2022-03-22 14:13:56 +0100

    Cargo fmt

--- src/config.rs
+++ src/config.rs
@@ -249,7 +249,8 @@ impl CoreConfig {
     ) -> Result<StartRequestAuthOnly, Error> {
         let decoded = decode_with_verifier_selector(request_jwt, |header| {
             Ok(header
-                .key_id().and_then(|kid| self.authonly_request_keys.get(kid))
+                .key_id()
+                .and_then(|kid| self.authonly_request_keys.get(kid))
                 .map(|key| key.as_ref()))
         })?
         .0;
